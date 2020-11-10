2d990e25a0f634a3338d3ed7f244c7f1a045fd85
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-11-09 21:07:26 -0500

    REmove logic from DOM

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,7 @@
+/* eslint-disable max-len */
+
+import { btnCLogic, btnFLogic, capital, errorD } from "./logic";
+
 const dom = () => {
   const cont = document.getElementById('cont');
   const buttonF = document.getElementById('changeF');
@@ -24,25 +28,20 @@ const dom = () => {
         error.style.display = 'none';
         cont.style.display = 'flex';
         temp.textContent = `${item.main.temp} C°`;
-        summary.textContent = item.weather[0].description.charAt(0).toUpperCase() + item.weather[0].description.slice(1);
+        summary.textContent = capital(item.weather[0].description);
         city.textContent = item.name;
         icon.src = `http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`;
 
         buttonF.addEventListener('click', () => {
-          temp.textContent = `${((parseFloat(item.main.temp) * (9 / 5)) + 32).toFixed(2)} F°`;
-          buttonF.style.display = 'none';
-          buttonC.style.display = 'block';
+          btnCLogic(item);
         });
 
         buttonC.addEventListener('click', () => {
-          temp.textContent = `${item.main.temp} C°`;
-          buttonF.style.display = 'block';
-          buttonC.style.display = 'none';
+          btnFLogic(item);
         });
       })
       .catch(() => {
-        cont.style.display = 'none';
-        error.style.display = 'block';
+        errorD();
       });
     form.reset();
   });
