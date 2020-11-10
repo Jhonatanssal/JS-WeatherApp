f6a654e65ec921c07db17147fdf6089021185b38
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-11-09 19:57:17 -0500

    Add buttons functionality

--- src/dom.js
+++ src/dom.js
@@ -1,10 +1,13 @@
 const dom = () => {
   const cont = document.getElementById('cont');
+  const buttonF = document.getElementById('changeF');
+  const buttonC = document.getElementById('changeC');
   const temp = document.getElementById('temperature');
   const city = document.getElementById('city');
   const summary = document.getElementById('summary');
   const icon = document.getElementById('icon');
   const form = document.getElementById('form');
+  const error = document.getElementById('error');
 
   const btn = document.getElementById('button');
 
@@ -18,14 +21,33 @@ const dom = () => {
     fetch(api, { mode: 'cors' })
       .then(response => response.json())
       .then(item => {
+        error.style.display = 'none';
         cont.style.display = 'flex';
         temp.textContent = `${item.main.temp} C°`;
         summary.textContent = item.weather[0].description.charAt(0).toUpperCase() + item.weather[0].description.slice(1);
         city.textContent = item.name;
         icon.src = `http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`;
-      });
+
+        buttonF.addEventListener('click', () => {
+          temp.textContent = `${((parseFloat(item.main.temp) * 9/5) + 32).toFixed(2)} F°`;
+          console.log((parseFloat(item.main.temp) * 9/5) + 32);
+          buttonF.style.display = 'none';
+          buttonC.style.display = 'block';
+        });
+
+        buttonC.addEventListener('click', () => {
+          temp.textContent = `${item.main.temp} C°`;
+          buttonF.style.display = 'block';
+          buttonC.style.display = 'none';
+        })
+      })
+      .catch(() => {
+        cont.style.display = 'none';
+        error.style.display = 'block';
+      })
     form.reset();
   });
 };
 
 export default dom;
+
